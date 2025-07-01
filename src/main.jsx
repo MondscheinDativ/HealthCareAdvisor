92d00bda2a6d05626ae43fc457a777aea0ef419e
Francis-Li20250603 <liben19960927@163.com>
2025-07-02 00:19:58 +0800

    Create main.jsx
    Entrance

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css'; // 后续引入 Tailwind 基础样式
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
